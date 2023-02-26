2074e249ef1a8ff8cb897ba08d51a54454a19e61
lomteankita <aankitalomte@gmail.com>
2023-02-26 10:50:34 +0530

    final

--- shoes/src/Components/Navbar.jsx
+++ shoes/src/Components/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from "react";
-import Card from "../Pages/Card";
-import Home from "../Pages/Home";
-import Jeans from "../Pages/Jeans";
-import Mens from "../Pages/Mens";
-import Women from "../Pages/Women";
-import Login from "../Pages/Login";
-
-import { Route,Routes } from "react-router-dom";
+import React from 'react'
+import {Link} from "react-router-dom";
 
 
+const Navbar = () => {
+const Links =[
+    {path:"/" , title:"Home"},
+    
+    {path:"/mens" ,title:"Mens"},
+    {path:"/women",title:"Womens"},
+    { path:"/jeans" ,title:"Jeans"},
+    { path:"/card", title:"Card"},
+    { path:"/login",title:"Login"},
+   
+    
+]
 
-const AllRoutes = () => {
   return (
-    <Routes>
-        <Route path="/" element={<Home />}> </Route>
-        <Route path="/card" element={<Card />}> </Route>
-        <Route path="/jeans" element={<Jeans />}> </Route>
-        <Route path="/mens" element={<Mens />}> </Route>
-        <Route path="/login" element={<Login />}> </Route>
-        <Route path="/women" element={<Women />}> </Route>
-    </Routes>
+
+    <div style={{
+        width:"80%",
+        display:"flex",
+        justifyContent:"space-arround",
+        margin:"auto",
+    }}>
+        {Links.map((link)=>(
+            <Link  style={{margin:"auto",color:"darkblue",fontSize:"25px"}} key={link.path} to={link.path}>
+                {link.title}
+            </Link>
+        ))}
+    </div>
   )
 }
 
-export default AllRoutes
+export default Navbar
